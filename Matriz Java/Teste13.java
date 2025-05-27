9d1ccfd4ff433db68b8cab7d15e89bd74e989f85
Guilherme Santos <150138164+GuilhermeSBS10@users.noreply.github.com>
2025-05-27 14:52:54 -0300

    Update Teste13.java
    testando

--- Matriz Java/Teste13.java
+++ Matriz Java/Teste13.java
@@ -17,7 +17,7 @@ public class Teste13 {
             }
         }
 
-        //Looping que percorre a matriz e mostra cada linha e coluna
+        //Looping que percorre a matriz e mostra cada linha e coluna!
         for(int a = 0; a < 3; a++){
             for(int b = 0; b < 3; b++){
                 System.out.printf("%2d | ", matriz[a][b]);
